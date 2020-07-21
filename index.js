3bfa1faa109aad30458918e0256822c018db7f66
Davida Gaffney <gdavida@gmail.com>
2020-07-21 00:46:50 -0500

    add empty mainText field bc Amazon wouldn't say without one...

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ server.get('/', (req, res) => {
 			    "uid": "PICKLE_FAIRY_JSON_TTS_1",
 			    "updateDate": "2020-07-19T00:00:00.0Z",
 			    "titleText": "A joke from the Pickle Fairy",
+			    "mainText":"",
 			    "streamURL": "https://picklefairy.s3.us-east-2.amazonaws.com/Pickle-fairy-test-1.mp3",
 			    "redirectionUrl": ""
 			   }
