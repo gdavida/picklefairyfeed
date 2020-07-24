b79fb2042483b3b0ac8fa294ee889539b6f93ee4
Davida Gaffney <gdavida@gmail.com>
2020-07-24 11:08:07 -0500

    date update

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ server.get('/', (req, res) => {
 	    [
 			  {
 			    "uid": "PICKLE_FAIRY_JSON_TTS_1",
-			    "updateDate": "2020-07-19T00:00:00.0Z",
+			    "updateDate": "2020-07-24T00:00:00.0Z",
 			    "titleText": "A joke from the Pickle Fairy",
 			    "streamURL": "https://picklefairy.s3.us-east-2.amazonaws.com/Pickle-fairy-test-1.mp3",
 			   },
 
 			  {
 			    "uid": "PICKLE_FAIRY_JSON_TTS_2",
-			    "updateDate": "2020-07-19T02:00:00.0Z",
+			    "updateDate": "2020-07-23T02:00:00.0Z",
 			    "titleText": "A joke from the Pickle Fairy",
 			    "mainText":"What do young pickles like on their birthday cake... Canned dills!",
 			   }
